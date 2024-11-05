a5b08fb388d1c7acfb4b7a193421833ac71e0d2c
wuyuliang <2331336537@qq.com>
2024-11-05 14:46:55 +0800

    feat: 添加分享与下载文件

--- src/ReactPlayground/PlaygroundProvider.tsx
+++ src/ReactPlayground/PlaygroundProvider.tsx
@@ -1,12 +1,21 @@
-import { PropsWithChildren, useState } from "react";
+import { PropsWithChildren, useEffect, useState } from "react";
 import { Files, PlaygroundContext, Theme } from "./PlaygroundContext";
-import { fileName2Language } from "./util";
+import { compress, fileName2Language, uncompress } from "./util";
 import { initFiles } from "./files";
 
 export const PlaygroundProvider = (props: PropsWithChildren) => {
   const { children } = props
-  console.log(initFiles, 6666);
-  const [files, setFiles] = useState<Files>(initFiles)
+  const getFilesFromUrl = () => {
+    let files: Files | undefined
+    try {
+        const hash = uncompress(window.location.hash.slice(1))
+        files = JSON.parse(hash)
+    } catch (error) {
+      console.error(error)
+    }
+    return files
+  }
+  const [files, setFiles] = useState<Files>(getFilesFromUrl() || initFiles)
   const [selectedFileName, setSelectedFileName] = useState('App.tsx');
   const [theme, setTheme] = useState<Theme>('dark');
 
@@ -23,7 +32,10 @@ export const PlaygroundProvider = (props: PropsWithChildren) => {
     delete files[name]
     setFiles({ ...files })
   }
-
+  useEffect(() => {
+    const hash = compress(JSON.stringify(files))
+    window.location.hash = hash
+}, [files])
   const updateFileName = (oldFieldName: string, newFieldName: string) => {
     if (!files[oldFieldName] || newFieldName === undefined || newFieldName === null) return
     const { [oldFieldName]: value, ...rest } = files
--- src/ReactPlayground/components/Header.tsx
+++ src/ReactPlayground/components/Header.tsx
@@ -1,35 +1,57 @@
-import styles from './index.module.scss'
+import styles from "./index.module.scss";
 
-import logoSvg from './icons/logo.svg';
-import { useContext } from 'react';
-import { MoonOutlined, SunOutlined } from '@ant-design/icons';
-import { PlaygroundContext } from '../PlaygroundContext';
+import logoSvg from "./icons/logo.svg";
+import { useContext } from "react";
+import {
+  DownloadOutlined,
+  MoonOutlined,
+  ShareAltOutlined,
+  SunOutlined,
+} from "@ant-design/icons";
+import { PlaygroundContext } from "../PlaygroundContext";
+import copy from "copy-to-clipboard";
+import { message } from "antd";
+import { downloadFiles } from "../util";
 
 export default function Header() {
-  const { theme, setTheme } = useContext(PlaygroundContext)
+  const { theme, setTheme, files } = useContext(PlaygroundContext);
 
   return (
     <div className={styles.header}>
       <div className={styles.logo}>
-        <img alt='logo' src={logoSvg}/>
+        <img alt="logo" src={logoSvg} />
         <span>React Playground</span>
       </div>
       <div className={styles.links}>
-        {theme === 'light' && (
+        {theme === "light" && (
           <MoonOutlined
-            title='切换暗色主题'
+            title="切换暗色主题"
             className={styles.theme}
-            onClick={() => setTheme('dark')}
+            onClick={() => setTheme("dark")}
           />
         )}
-        {theme === 'dark' && (
+        {theme === "dark" && (
           <SunOutlined
-            title='切换亮色主题'
+            title="切换亮色主题"
             className={styles.theme}
-            onClick={() => setTheme('light')}
+            onClick={() => setTheme("light")}
           />
         )}
+        <ShareAltOutlined
+          style={{ marginLeft: "10px" }}
+          onClick={() => {
+            copy(window.location.href);
+            message.success("分享链接已复制。");
+          }}
+        />
+        <DownloadOutlined
+          style={{ marginLeft: "10px" }}
+          onClick={async () => {
+            await downloadFiles(files);
+            message.success("下载完成");
+          }}
+        />
       </div>
     </div>
-  )
+  );
 }
